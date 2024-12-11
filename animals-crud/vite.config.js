5fcec811a23b71e0288a361d17f326129d385a65
Doc-UP-AlejandroJaimes <Doc-UP-AlejandroJaimes>
2024-12-11 00:25:09 -0500

    chore(config): update Vite configuration
    - Modified `vite.config.js` for improved project setup and compatibility

--- animals-crud/vite.config.js
+++ animals-crud/vite.config.js
@@ -1,19 +1,15 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-// export default defineConfig({
-//   plugins: [svelte()],
-// });
-
 export default defineConfig({
   plugins: [svelte()],
-  server: {
-    proxy: {
-      '/v1': {
-        target: 'https://ljr9in3d88.execute-api.us-east-1.amazonaws.com/Prod/v1',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/v1/, ''),
-      },
+  build: {
+    outDir: 'dist', // Directorio de salida
+    emptyOutDir: true, // Limpia el directorio antes de construir
+  },
+  resolve: {
+    alias: {
+      '@': '/src', // Configuración opcional para usar '@' como alias a 'src'
     },
   },
 });
